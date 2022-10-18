6bcc3a477cdb699504e445846e41e3f37148cb28
Kumaravel V <75235334+Kumaravel655@users.noreply.github.com>
2022-10-18 15:59:05 +0530

    Add files via upload

---
+++ new/new/app/src/main/java/com/example/anew/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.anew;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.os.Bundle;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+
+import android.view.View;
+import android.widget.TextView;
+import java.util.List;
+public class MainActivity extends AppCompatActivity {
+    private SensorManager mgr;
+    private TextView txtList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mgr = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        txtList = (TextView)findViewById(R.id.sensorslist);
+        List<Sensor> sensorList = mgr.getSensorList(Sensor.TYPE_ALL);
+        StringBuilder strBuilder = new StringBuilder();
+        for(Sensor s: sensorList){
+            strBuilder.append(s.getName()+"\n");
+        }
+        txtList.setVisibility(View.VISIBLE);
+        txtList.setText(strBuilder);
+    }
+}
